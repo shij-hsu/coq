911a5de1e38d1f7e267677ca3e448c9b30d7aef4
xushijie95 <xushijie95@outlook.com>
2016-12-03 02:06:58 +0800

    掌握了coqdoc的使用方法，位于tac/pdf目录下. 2016/12/2. Kyo.

--- sf/Typechecking.v.d
+++
@@ -1,2 +0,0 @@
-Typechecking.vo Typechecking.glob Typechecking.v.beautified: Typechecking.v ./Maps.vo ./Smallstep.vo ./Stlc.vo
-Typechecking.vio: Typechecking.v ./Maps.vio ./Smallstep.vio ./Stlc.vio
